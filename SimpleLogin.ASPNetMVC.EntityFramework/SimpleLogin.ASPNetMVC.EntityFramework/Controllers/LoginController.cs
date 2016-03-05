ccdd342d21477cd6f228c005aacaa9ef4fa1ef64
Danilo Valim <danilovalim@hotmail.com>
2016-03-05 13:53:28 -0300

    First Commit

---
+++ SimpleLogin.ASPNetMVC.EntityFramework/SimpleLogin.ASPNetMVC.EntityFramework/Controllers/LoginController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleLogin.ASPNetMVC.EntityFramework.Models;
+
+namespace SimpleLogin.ASPNetMVC.EntityFramework.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            if(Session["FirstName"]!=null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User u)
+        {
+            if(ModelState.IsValid)
+            {
+                using (LoginContext db = new LoginContext())
+                {
+                    db.user.Add(u);
+                    db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+            }
+            else
+            {
+                return View(u);
+            }
+            
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            if(ModelState.IsValid)
+            {
+                using (LoginContext db = new LoginContext())
+                {
+                    User visitant = db.user.Where(x => x.UserName.Equals(u.UserName) && x.Password.Equals(u.Password)).FirstOrDefault();
+
+                    if (visitant != null)
+                    {
+                        Session["FirstName"] = visitant.FirstName;
+                        Session["LastName"] = visitant.LastName;
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View(u);
+                    }
+                }
+            }
+            else
+            {
+                return View(u);
+            }
+
+            
+
+                
+        }
+    }
+}
---
+++ SimpleLogin.ASPNetMVC.EntityFramework/SimpleLogin.ASPNetMVC.EntityFramework/Models/LoginContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleLogin.ASPNetMVC.EntityFramework.Models
+{
+    public class LoginContext : DbContext
+    {
+        public DbSet<User> user { get; set; }
+    }
+}
